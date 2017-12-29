04d5c5b97e13a22e7b11e9d509e5b4f13da94aa6
skhye05 <mayunga.j@gmail.com>
2017-12-29 02:21:49 +0200

    design blog page

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -7,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogComponent implements OnInit {
 
-  constructor() { }
+  public posts: any[];
+
+  constructor() {
+    this.posts = new Array<Object>();
+
+    for (let i = 0; i < 6; i++) {
+      this.posts.push({
+        image: 'assets/img/blank.svg',
+        title: 'Post ' + (i + 1),
+        date: 'Octobe 2017',
+        text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.'
+      });
+    }
+  }
 
   ngOnInit() {
   }
